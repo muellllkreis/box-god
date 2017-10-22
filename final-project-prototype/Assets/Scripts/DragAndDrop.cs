f89ad69388dbc6d07f5010fdfc86973438a70f2c
muellllkreis <rietig.m@googlemail.com>
2017-10-22 18:44:54 -0400

    added digital prototype

---
+++ final-project-prototype/Assets/Scripts/DragAndDrop.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragAndDrop : MonoBehaviour {
+
+    Vector3 dist;
+    float posX;
+    float posY;
+    Rigidbody rb;
+    Camera main;
+    bool active;
+
+    private void Start()
+    {
+        this.active = true;
+        rb = GetComponent<Rigidbody>();
+        rb.useGravity = false;
+        main = GameObject.Find("MainCamera").GetComponent<Camera>();
+    }
+
+    void OnMouseDown()
+    {
+        if(active)
+        {
+            dist = main.WorldToScreenPoint(transform.position);
+            posX = Input.mousePosition.x - dist.x;
+            posY = Input.mousePosition.y + dist.y;
+        }
+    }
+
+    private void OnMouseDrag()
+    {
+        if (active)
+        {
+            Vector3 curPos = new Vector3(Input.mousePosition.x - posX, Input.mousePosition.y - posY, dist.z);
+            Vector3 worldPos = main.ScreenToWorldPoint(curPos);
+            transform.position = worldPos;
+        }
+    }
+
+    private void OnMouseUp()
+    {
+        rb.useGravity = true;
+        rb.mass = 1000;
+        this.active = false;
+        gameObject.layer = LayerMask.NameToLayer("3D GUI");
+    }
+}
---
+++ final-project-prototype/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    private Rigidbody rb;
+    RaycastHit hit;
+    Vector3 size;
+    public Vector3 jump;
+    public float jumpForce = 0.1f;
+    public float springJumpForce = 10000000.3f;
+    private float distToGround;
+    public Vector3 movement;
+    public LayerMask ignoreLayer;
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+        size = GetComponent<Collider>().bounds.size;
+        jump = new Vector3(0.0f, 2.0f, 0.0f);
+        movement = new Vector3(speed, 0.0f, 0.0f);
+        distToGround = GetComponent<Collider>().bounds.extents.y;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position += new Vector3(speed, 0.0f, 0.0f) * Time.deltaTime;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Plank")
+        {
+            rb.AddForce(jump * jumpForce, ForceMode.Impulse);
+            collision.gameObject.tag = "used";
+        }
+        if (collision.gameObject.tag == "Spring")
+        {
+            rb.AddForce(jump * springJumpForce, ForceMode.Impulse);
+            collision.gameObject.tag = "used";
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Plank")
+        {
+        }
+    }
+
+    bool isGrounded()
+    {
+        return Physics.Raycast(transform.position, -Vector3.up, distToGround + 0.1f);
+    }
+}
