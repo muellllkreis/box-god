0240f6d529fda6756f3c86326f487e088263e075
mjl4nk <mjl4nk@virginia.edu>
2017-11-05 11:24:42 -0500

    menu-commit
    Added a pause menu which can be activated by the esc or space keys.  From this menu you can choose to continue the level, restart the level, or quit the game.
    
    Also added a portal object at the end of each level that will load the next scene of the game.

---
+++ final-project-prototype/Assets/Scripts/GameManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+	public Transform canvas;
+	public float menuDelay = 1.0f;
+	public static GameManager instance = null;
+
+	// Use this for initialization
+	void Start () {
+		canvas.gameObject.SetActive (false);
+		Time.timeScale = 1.0f;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//code for pausing the game when esc or space are pressed
+		if (Input.GetKeyDown (KeyCode.Escape) || Input.GetKeyDown(KeyCode.Space)) {
+			if (canvas.gameObject.activeInHierarchy == false) {
+				canvas.gameObject.SetActive (true);
+				Time.timeScale = 0.0f;
+			} else {
+				canvas.gameObject.SetActive (false);
+				Time.timeScale = 1.0f;
+			}
+		}
+
+
+	}
+
+	public void ContinueLevel() {
+		canvas.gameObject.SetActive (false);
+		Time.timeScale = 1.0f;
+	}
+
+	public void RestartLevel() {
+		SceneManager.LoadScene (SceneManager.GetActiveScene ().name);
+	}
+
+	public void QuitGame() {
+		Application.Quit ();
+	}
+
+}
---
+++ final-project-prototype/Assets/Scripts/NextLevelPortal.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextLevelPortal : MonoBehaviour {
+
+	public void OnTriggerEnter(Collider collider) {
+		if (collider.gameObject.tag == "Player") {
+			int levelNum = SceneManager.GetActiveScene ().buildIndex;
+			levelNum++;
+			SceneManager.LoadScene (levelNum);
+
+		}
+	}
+
+}
